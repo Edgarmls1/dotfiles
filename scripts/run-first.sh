f2697108ae9cb6e47c80c6bffefad43af2dde2b4
Edgar Salazar <129508613+Edgarmls1@users.noreply.github.com>
2025-09-17 10:12:37 -0300

    Update run-first.sh

--- scripts/run-first.sh
+++ scripts/run-first.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/bash
+#! /bin/bash
 
 AUR() {
     if ! command -v yay &> /dev/null; then
@@ -12,20 +12,15 @@ AUR() {
     fi
 }
 
-conda_config () {
-    echo "Configurando o Anaconda..."
-    cd /home/$USER/
-    sudo pacman -Sy --noconfirm libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl alsa-lib libglvnd
-    if [ ! -f ~/Anaconda3-2025.06-1-Linux-x86_64.sh ]; then
-        curl -O https://repo.anaconda.com/archive/Anaconda3-2025.06-1-Linux-x86_64.sh || { echo "Falha ao baixar o Anaconda!"; exit 1; }
-    fi
-    bash ~/Anaconda3-2025.06-1-Linux-x86_64.sh || { echo "Falha ao instalar o Anaconda!"; exit 1; }
-    source ~/anaconda3/bin/activate
-    conda init
+bluetooth() {
+    sudo pacman -S --noconfirm bluez bluez-utils blueberry
+    sudo systemctl enable bluetooth
+    sudo sed -i 's/^#AutoEnable=true/AutoEnable=true/' /etc/bluetooth/main.conf
+    sudo systemctl restart bluetooth
 }
 
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
 
 AUR
-conda_config
+bluetooth
