d6c809ddf5b909f4f87cac6de481e2f242bb45bb
Edgar Salazar <129508613+Edgarmls1@users.noreply.github.com>
2025-09-17 12:18:59 -0300

    Update dotfiles-requirements.sh

--- scripts/dotfiles-requirements.sh
+++ scripts/dotfiles-requirements.sh
@@ -1,4 +1,57 @@
-#! /usr/bin/bash
-sudo pacman -S hyprland hyprpaper hyprshot kitty nemo rofi waybar swaync fastfetch zsh yazi blueberry neovim
+#! /bin/bash
+sudo pacman -S hyprland hyprpaper hyprsunset hyprlock hyprshot kitty nautilus rofi waybar swaync fastfetch yazi blueberry neovim gedit ttf-jetbrains-mono-nerd qt6ct gnome-tweaks ly firefox lsd fzf
 
 yay -S hyprsome-git mpvpaper-git wlogout
+
+git clone https://github.com/mjkim0727/Pop-Extended.git
+mkdir -p ~/.icons
+cd Pop-Extended
+mv Pop-Extended ~/.icons/pop
+cd
+git clone https://github.com/ChrisTitusTech/Top-5-Bootloader-Themes.git
+
+rm ~/.bashrc && mv ~/dotfiles/.bashrc ~/.bashrc
+mv ~/dotfiles/scripts/sys_update.sh ~/sys_update.sh
+
+clear 
+echo """
+  installed packages:
+    # lockscreen
+      - ly (run 'systemctl enable ly.service' and reboot)
+    # hyprland
+      - hyprland -> window manager
+      - hyprpaper -> wallpaper
+      - hyprsunset -> blue light filter
+      - hyprlock -> lockscreen
+      - hyprshot -> printscreen
+      - hyprsome -> multiple monitors
+    # browser
+      - firefox
+    # terminal
+      - kitty
+    # file manager
+      - nautilus -> GUI
+      - yazi -> TUI
+    # apps launcher
+      - rofi
+    # status bar
+      - waybar
+    # notification daemon
+      - swaync
+    # text editor
+      - neovim -> TUI
+      - gedit -> GUI
+    # bluetooth manager
+      - blueberry
+    # theme switcher
+      - qt6ct
+      - gnome tweaks
+    # logout menu
+      - wlogout
+    # animated wallpaper
+      - mpvpaper
+    # others
+      - lsd
+      - fzf
+      - JetBrains nerd font
+"""
