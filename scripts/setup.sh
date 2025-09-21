c3f56b331b18f87ce0c60ea3d7d443f2ee47bd95
Edgar Salazar <129508613+Edgarmls1@users.noreply.github.com>
2025-09-21 10:04:44 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -90,7 +90,7 @@ dev() {
     echo "| instalando config p/ dev |"
     echo "+--------------------------+"
 
-    sudo pacman -S neovim ttf-jetbrains-mono-nerd obsidian bitwarden
+    sudo pacman -S neovim ttf-jetbrains-mono-nerd obsidian bitwarden gnome-boxes
     yay -S visual-studio-code-bin
 
     read -p "Deseja instalar o java? (s/N)" java
