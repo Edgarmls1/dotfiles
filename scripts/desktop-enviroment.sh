86c5e437085bf76a027a16d94230439770160278
Edgar Salazar <129508613+Edgarmls1@users.noreply.github.com>
2025-10-08 17:41:53 -0300

    Update desktop-enviroment.sh

--- scripts/desktop-enviroment.sh
+++ scripts/desktop-enviroment.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 sudo pacman -S hyprland hyprpaper hyprsunset hyprlock hyprshot kitty nautilus \
 rofi waybar swaync fastfetch yazi blueberry pavucontrol network-manager-applet \
-neovim gedit ttf-jetbrains-mono-nerd qt6ct gnome-tweaks ly firefox lsd fzf htop \
+neovim gedit ttf-hack-nerd qt6ct gnome-tweaks ly firefox lsd fzf htop \
 btop cava bat npm evince xdg-desktop-portal-gtk xdg-desktop-portal-hyprland syncthing os-prober
 
 yay -S hyprsome-git mpvpaper-git wlogout qimgv
