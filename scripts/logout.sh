5191928f672aa0f56e7528ac995a2ad63c96e64c
Edgarmls1 <edgar1macedosalazar@gmail.com>
2025-11-04 19:42:31 -0300

    update 04/11/25

--- scripts/logout.sh
+++ scripts/logout.sh
@@ -3,8 +3,11 @@
 chosen=$(printf "Hibernate\nPower Off\nRestart" | rofi -dmenu -i -p "" -theme "~/.config/rofi/power.rasi")
 
 case "$chosen" in 
-	"Power Off") systemctl poweroff     ;;
-	"Restart") systemctl reboot         ;;
-	"Hibernate") systemctl hybrid-sleep ;;
-	*) exit 1                           ;;
+	"Power Off") systemctl poweroff ;;
+	"Restart") systemctl reboot     ;;
+	"Hibernate")
+		hyprlock &
+		systemctl hybrid-sleep 
+	;;
+	*) exit 1                       ;;
 esac
